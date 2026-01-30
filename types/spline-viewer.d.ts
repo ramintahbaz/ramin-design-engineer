7708a3bf1ee440b163141b0f0646a8096a038176
ramintahbaz23 <ramin@pattern23.ai>
2026-01-30 11:02:28 -0500

    build update

--- types/spline-viewer.d.ts
+++ types/spline-viewer.d.ts
@@ -1,11 +1,17 @@
-declare namespace JSX {
-  interface IntrinsicElements {
-    'spline-viewer': React.DetailedHTMLProps<
-      React.HTMLAttributes<HTMLElement> & {
-        url?: string;
-      },
-      HTMLElement
-    >;
+import 'react';
+
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      'spline-viewer': React.DetailedHTMLProps<
+        React.HTMLAttributes<HTMLElement> & {
+          url?: string;
+        },
+        HTMLElement
+      >;
+    }
   }
 }
+
+export {};
 
