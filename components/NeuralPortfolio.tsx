d717049732134f24640b355b9a7a215fd7156b20
ramintahbaz23 <ramin@pattern23.ai>
2026-03-19 15:26:35 -0400

    typescript fix

--- components/NeuralPortfolio.tsx
+++ components/NeuralPortfolio.tsx
@@ -1832,7 +1832,7 @@ export default function NeuralPortfolio({ isLayerVisible = true, splashMode = fa
     }
 
     let idleHandle: number | undefined;
-    let fallbackHandle: ReturnType<typeof window.setTimeout> | undefined;
+    let fallbackHandle: number | undefined;
     let cleanupPromise: Promise<(() => void) | void> | undefined;
 
     const runInit = () => {
